71a307e76209e470cac070aa85ba5a5dbdde08f6
hdonahue914 <Hwdonahue@gmail.com>
2023-10-26 03:28:24 +0000

    tried to fix placeholder again

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -50,7 +50,7 @@ const ItemPreview = (props) => {
           <Link to={`/@${item.seller.username}`} className="flex-grow-1">
             <img
               src={item.seller.image}
-              onerror="this.src='./placeholder.png'"
+              onerror={this.src='./placeholder.png'}
               alt={item.seller.username}
               className="user-pic rounded-circle pr-1"
             />
